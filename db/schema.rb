fdb98da522d94dbeeeaf2469cf3db186f1bf980b
Justin Wong <justincwong89@gmail.com>
2016-07-21 16:40:13 -0700

    Switch to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160721182336) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "newspapers", force: :cascade do |t|
     t.string   "publication"
     t.string   "main_phone"
